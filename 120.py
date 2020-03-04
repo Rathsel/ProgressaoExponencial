47a8b83dc9dd7976687ddd246a23ac4d69f610c5
Thiago Martins <60950009+Rathsel@users.noreply.github.com>
2020-03-03 22:02:02 -0300

    first commit

---
+++ 120.py
@@ -0,0 +1,9 @@
+#autores: 1DSI3 Thiago Martins
+#feito: 03/03/2020
+#instituição: Etec
+x = 0 #declaração do contador
+valor = 2 #eclaração da variavel/valor
+while (x < 21):
+		valor = valor * valor
+		x = x + 1 	#laço de repetição
+print (valor)		#exibir a variavel valor
